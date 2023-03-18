6874bc529ce5ff9fc86c22c35ec6c548f285b139
isahua16 <isaelhuard@gmail.com>
2023-03-18 10:24:49 -0600

    playing with while loops

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let counter = 0;
+while(counter <= 50) {
+    console.log(counter);
+    counter++;
+}
+
+let counter_two = 0;
+while(counter_two >= -50) {
+    console.log(counter_two);
+    counter_two--;
+}
+
+let counter_three = 2;
+while(counter_three <= 100) {
+    console.log(counter_three);
+    counter_three = counter_three + 2;
+}
+
